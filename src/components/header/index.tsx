7220618ad6308f9473e0f4f57e1dbaadcdfac2c0
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-06-05 17:08:40 -0300

    add: favicon

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -44,7 +44,7 @@ export function Header() {
                 )}
 
                 <Image
-                    src="/Logo.svg"
+                    src="worldtriplogo.svg"
                     alt="logo world trip"
                     justifySelf="center"
                     gridColumn={2}
